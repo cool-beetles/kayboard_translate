ef09ce9e3f6fbdffeddb1177507b3eff9ea37408
Ada <adrianna.kucinska@onet.eu>
2017-10-29 19:29:19 +0100

    Add numbers to hash's arrays

--- bin/start.rb
+++ bin/start.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 
-require './phone.rb'
+require '../phone.rb'
 
 phone = Phone.new
 
+puts "Select contact:"
 input_message = gets.chomp
 puts phone.string_to_num(input_message)
